2dac43b743644bd530629b2840a4a06f01a1e3ab
asansalone <sansalone.alejandro@gmail.com>
2020-11-26 10:10:42 -0300

    Leccion 6 Javascript V (Clases )- realizada

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -3,11 +3,23 @@
 function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
-  // En el `contructor`, define el usuario, el nombre, el email y la contraseña
-  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // En el `constructor`, define el usuario, el nombre, el email y la contraseña
+  // El `constructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+    function Usuario(opciones) {
+      this.usuario =  opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;     
+    }; 
+
+    Usuario.prototype.saludar = function() {
+      return 'Hola, mi nombre es ' + this.nombre;
+    };
+
+    return Usuario;
 
 }
 
@@ -15,6 +27,9 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function() {
+    return "Hello World!";
+  }
 
 }
 
@@ -25,6 +40,18 @@ function agregarStringInvertida() {
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
 
+// for(var x = 0; x < array.length; x++)
+
+  String.prototype.reverse = function () {
+    
+    var cadena = '';
+    for(var x = this.length-1; x >= 0; x-- ) {
+      cadena = cadena + this.charAt(x);
+    }
+    return cadena;
+    
+  }
+  
 }
 
 // No modificar nada debajo de esta línea
